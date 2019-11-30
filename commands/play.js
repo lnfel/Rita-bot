ff9b8c273da569a3e1adfc59d0acd8cd8da2d06d
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2019-12-01 06:53:47 +0800

    Youtube search initialized

--- commands/play.js
+++ commands/play.js
@@ -31,9 +31,36 @@ module.exports = {
 			let embedMsg = await message.channel.send(embed);
 			let filter = m => m.author.id === message.author.id;
 			let query = await message.channel.awaitMessages(filter, { max: 1 });
-			let results = await search(query, opts).catch(err => console.log(err));
+			let results = await search(query.first().content, opts).catch(err => console.log(err));
+			if (results){
+				let youtubeResults = results.results;
+				let i = 0;
+				let titles = youtubeResults.map(result => {
+					i++;
+					return i + ") " + result.title;
+				})
+				console.log(titles);
+				message.channel.send({
+					embed: {
+						title: 'Select a song by entering the number.',
+						description: titles.join("\n")
+					}
+				}).catch(err => console.log(err));
 
-			console.log(results);
+				filter = m => (m.author.id === message.author.id) && m.content >= 1 && m.content <= youtubeResults.length;
+				let collected = await message.channel.awaitMessages(filter, { maxMatches: 1 });
+				let selected = youtubeResults[collected.first().content - 1];
+
+				embed = new Discord.RichEmbed()
+					.setTitle(`${selected.title}`)
+					.setURL(`${selected.link}`)
+					.setDescription(`${selected.description}`)
+					.setThumbnail(`${selected.thumbnails.default.url}`);
+
+				message.channel.send(embed);
+			}
+
+			//console.log(results);
 			//console.log(query.first().content);
 		};
 		// run youtubeSearch function
